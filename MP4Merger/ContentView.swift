9b4cb615244388c5c3165871849b2fc7b8c5d7a7
Yoichi Yamane <yochan03@gmail.com>
2025-12-31 23:23:08 +0900

    Initial Commit

---
+++ MP4Merger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MP4Merger
+//
+//  Created by 山根洋一 on 2025/12/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MP4Merger/MP4MergerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MP4MergerApp.swift
+//  MP4Merger
+//
+//  Created by 山根洋一 on 2025/12/31.
+//
+
+import SwiftUI
+
+@main
+struct MP4MergerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
