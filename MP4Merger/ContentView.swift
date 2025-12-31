539334993697078cff7b27bd78689c98e8ca014c
yamachan03 <20498485+yamachan03@users.noreply.github.com>
2025-12-31 23:23:08 +0900

    Initial Commit

---
+++ MP4Merger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MP4Merger
+//
+//  Created by (redacted) on 2025/12/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MP4Merger/MP4MergerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MP4MergerApp.swift
+//  MP4Merger
+//
+//  Created by (redacted) on 2025/12/31.
+//
+
+import SwiftUI
+
+@main
+struct MP4MergerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
